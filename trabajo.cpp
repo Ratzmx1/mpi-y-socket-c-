248cd2029ef653f39dadcdcc8aa9b32f38d8a0ae
Dexter <luxo_v7@hotmail.com>
2021-07-14 23:05:39 -0400

    hice una wea penca

--- trabajo.cpp
+++ trabajo.cpp
@@ -6,6 +6,8 @@
 #include <map>
 #include <set>
 #include <mpi.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 using namespace std;
 
@@ -151,7 +153,37 @@ int main(int argc, char *argv[]){
 
 
 
+        int i, sockfd, socket_aceptado;
+        struct sockaddr_in my_addr;
+        struct sockaddr_in remote_addr;
+        int addrlen;
 
+
+        // Creacion del socket.
+        sockfd = socket (AF_INET, SOCK_STREAM, 0);
+
+        my_addr.sin_family = AF_INET; //Protocolo TCP
+        my_addr.sin_port = htons(3001); //Puerto
+        my_addr.sin_addr.s_addr = inet_addr ("127.0.0.1"); // IP por donde recibira paquetes el programa
+        //my_addr.sin_addr.s_addr = inet_addr ("192.168.101.68"); // IP por donde recibira paquetes el programa
+
+        // Asignando IP y puerto al socket
+        if (-1 == bind (sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr) ))
+        {
+            perror("bind");
+            exit(EXIT_FAILURE);
+        }
+
+        // Se habilita el socket para poder recibir conexiones.
+        if (-1 == listen(sockfd, 50))
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+    
+        // Se llama a accept() y el servidor queda en espera de conexiones. accept() es bloqueante.
+        addrlen = sizeof (struct sockaddr);
+        socket_aceptado = accept(sockfd, (struct sockaddr *)&remote_addr, (socklen_t *) &addrlen);
 
 
         cin >> cantidad;
